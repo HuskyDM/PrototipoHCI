549b8ccdc6e05879ba7f6cf9b5413153dd8fe8e7
SebastianCabreraZamora <sebastiancz17@gmail.com>
2018-11-11 18:52:40 -0600

    Prueba de javas en el directorio anterior a prototipohci

---
+++ app/src/main/java/com/proyecto/hci/DietasPrincipal.java
@@ -0,0 +1,14 @@
+package com.proyecto.hci;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DietasPrincipal extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dietas_principal);
+    }
+
+}
